f4f6b99dbf3ee41a862a82a752df1a4f167dcd99
Przemyslaw Kaminski <pkaminski@mirantis.com>
2014-12-10 07:30:08 +0100

    Make REQUESTS_DIR/SERVERS_DIR

--- src/settings.py
+++ src/settings.py
@@ -13,6 +13,12 @@ CONFIG_DIR = os.path.join(PROJECT_DIR, 'config')
 REQUESTS_DIR = os.path.join(CONFIG_DIR, 'requests')
 SERVERS_DIR = os.path.join(CONFIG_DIR, 'servers')
 
+if not os.path.exists(REQUESTS_DIR):
+    os.makedirs(REQUESTS_DIR)
+
+if not os.path.exists(SERVERS_DIR):
+    os.makedirs(SERVERS_DIR)
+
 CSS_STATICS_DIR = os.path.join(PROJECT_DIR, 'server', 'css')
 HTML_STATICS_DIR = os.path.join(PROJECT_DIR, 'server', 'html')
 JS_STATICS_DIR = os.path.join(PROJECT_DIR, 'server', 'js')
